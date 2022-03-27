7aae1816d1b2a907b109b3d4510a56aece2f4cc2
Joey Flores <61069122+Coalemus@users.noreply.github.com>
2022-03-28 03:55:45 +0800

    Added documentation

--- MERN CRUD/mern-stack-api/models/postmessage.js
+++ MERN CRUD/mern-stack-api/models/postmessage.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose")
 
-var postmessage = mongoose.model("postmessage", // defining the data structure of postmessage.
+
+// defining the data structure of postmessage.
+var postmessage = mongoose.model("postmessage", // First Parameter is MongoDB collection name.
 {
     title : {type:String},
     message : {type:String},
-})
+}, 'postmessages') // Added names to specify other names for the collection on MongoDB/first parameter.
 
 module.exports = {postmessage}
