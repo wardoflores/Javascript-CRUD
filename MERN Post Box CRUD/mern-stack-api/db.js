e130f071f1d559c6dca4844fe6db3760a9f72179
Joey Flores <61069122+Coalemus@users.noreply.github.com>
2022-05-28 15:45:46 +0800

    Fixed bug that hinders connection to the database

--- MERN Post Box CRUD/mern-stack-api/db.js
+++ MERN Post Box CRUD/mern-stack-api/db.js
@@ -2,7 +2,7 @@
 
 const mongoose = require("mongoose")
 
-mongoose.connect('mongodb://localhost:27017/postManagerDB',{useNewUrlParser:true,useUnifiedTopology:true},
+mongoose.connect('mongodb://127.0.0.1:27017/postManagerDB',{useNewUrlParser:true,useUnifiedTopology:true},
 err => {
     if(!err)
         console.log("MongoDB connection succeeded.")
