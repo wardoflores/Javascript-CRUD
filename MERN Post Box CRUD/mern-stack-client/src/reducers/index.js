931d3c5f9934259524188f76146e7d93ceb06ddd
wardoflores <61069122+wardoflores@users.noreply.github.com>
2023-03-22 14:32:43 +0800

    moved to React-Projects repository

--- MERN Post Box CRUD/mern-stack-client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-// referred by `store.js` for the reducers [6]
-
-import { combineReducers } from "redux";
-import { postMessage } from "./postMessage";
-
-export const reducers = combineReducers({
-    postMessage
-})
